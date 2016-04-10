357c205f226e4aab702edc9b97ff4ccd02b0d846
juan <theaxford@gmail.com>
2016-04-10 13:24:26 +0200

    Estracción de método amountFor

--- codigo/Customer.java
+++ codigo/Customer.java
@@ -31,25 +31,7 @@ class Customer {
 			double thisAmount = 0;
 			Rental each = (Rental) rentals.nextElement();
 
-			//determine amounts for each line
-			switch (each.getMovie.getPriceCode()) {
-
-				case Movie.REGULAR:
-					thisAmount += 2;
-					if (each.getDaysRented getDaysRented() > 2)
-						thisAmount += (each.getDaysRented() - 2) * 1.5;
-					break;
-
-					case Movie.NEW_RELEASE:
-						thisAmount += each.getDaysRented() * 3;
-					break;
-
-					case Movie.CHILDRENS:
-						thisAmount += 1.5;
-						if (each.getDaysRented() > 3)
-							thisAmount += (each.getDaysRented() - 3) * 1.5;
-						break;
-			}
+			thisAmount = amounFor(each);
 
 			// add frequent renter points
 			frequentRenterPoints ++;
@@ -67,4 +49,32 @@ class Customer {
 		result += "You earned " + String.valueOf(frequentRenterPoints) + " frequent renter points";
 		return result;
 	}
+
+	private double amounFor(Rental each){
+
+		double thisAmount = 0;
+
+		switch (each.getMovie.getPriceCode()) {
+
+			case Movie.REGULAR:
+				thisAmount += 2;
+				if (each.getDaysRented getDaysRented() > 2)
+					thisAmount += (each.getDaysRented() - 2) * 1.5;
+				break;
+
+				case Movie.NEW_RELEASE:
+					thisAmount += each.getDaysRented() * 3;
+				break;
+
+				case Movie.CHILDRENS:
+					thisAmount += 1.5;
+					if (each.getDaysRented() > 3)
+						thisAmount += (each.getDaysRented() - 3) * 1.5;
+				break;
+		}
+
+		return thisAmount;
+
+	}
+
 }
